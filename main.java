1f9e14b17d38e4b4fc1b26bd2e0ca90fbce938b0
Artha Uli <2381011@unai.edu>
2024-12-12 15:55:36 +0700

    Artha

--- main.java
+++ main.java
@@ -4,12 +4,11 @@ import view.ConsoleView;
 
 public class main {
     public static void main(String[] args) {
-        // Setup dependencies
+
         var patientRepository = new PatientRepositoryImpl();
         var patientService = new PatientService(patientRepository);
         var consoleView = new ConsoleView(patientService);
 
-        // Run the application
         consoleView.showMainMenu();
     }
 }
